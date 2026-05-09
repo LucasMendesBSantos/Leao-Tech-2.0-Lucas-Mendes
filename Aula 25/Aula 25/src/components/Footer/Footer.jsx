dcd784a2ea0a97c95d7bbc6efafcfb0adb7eb1d9
Lucas Mendes Brito <60691078378@espcid.intra>
2026-05-08 21:36:57 -0300

    Aulas de react e comentarios

---
+++ Aula 25/Aula 25/src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+function Footer(){
+    return(
+        <>
+        
+            <footer className=" bg-black text-light text-center p-3">
+                <div className=" container">
+                    <div className="row">
+                        <div className="col-4">
+                           <div>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                           </div>
+                           
+                        </div>
+                        <div className="col-4">
+                           <div>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                           </div>
+                           
+                        </div>
+                        <div className="col-4">
+                           <div>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                            <p>texto pequeno</p>
+                           </div>
+                           
+                        </div>
+                    </div>
+                </div>
+
+            </footer>
+
+        </>
+    )
+}
+export default Footer;
---
+++ Aula 25/Aula 25/src/pages/Sobre/Sobre.jsx
@@ -0,0 +1,9 @@
+function Sobre(){
+    return(
+        <>
+        <h1>Essa é a pagina Sobre</h1>
+        </>
+    )
+
+}
+export default Sobre;
